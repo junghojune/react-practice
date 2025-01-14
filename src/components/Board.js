e071d7febb54abdbe040d38445fdac7ebac83bde
hosu <76641246+junghojune@users.noreply.github.com>
2025-01-14 10:32:40 +0900

    승자 결정하는 로직 추가

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,11 +5,45 @@ const Board = () => {
     
     const[squares, setSquares] = useState(Array(9).fill(null));
     const [xIsNext, setxIsNext] = useState(true);
+    
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
 
-    const status = `Next player : ${xIsNext ? 'X' : 'O'}`;
+        for(let index = 0; index < lines.length; index++){
+            const [a,b,c] = lines[index];
+            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a];
+            }
+        }
 
+        return null;
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner){
+        status = 'Winner: ' + winner;
+    } else {
+        status = `Next player : ${xIsNext ? 'X' : 'O'}`;
+    }
+
+    
     const handleClick = (i) => {
         const newSquares = squares.slice();
+
+        if(calculateWinner(newSquares) || newSquares[i]) {
+            return;
+        }
+
         newSquares[i] = xIsNext ? 'X' : 'O';
         setSquares(newSquares);
         setxIsNext(prev => !prev)
@@ -20,7 +54,6 @@ const Board = () => {
             onClick={() => handleClick(i)} />
     }
  
-    
     return (
         <div>
             <div className='status'>{status}</div>
